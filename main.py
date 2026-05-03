ce7805e500614a84194cb751c922b3e7a0aa6561
SEscobar_10T <escobar.sa@obmontessori.edu.ph>
2026-05-04 07:13:32 +0800

    Change display target from 'output2' to 'output'

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def show_creds(e):
     student1 = StudentAddition(name, section, favsub)
     classlist.append(student1)
 
-    display(f'{student1.name} from {student1.section} loves the {student1.favsub} subject.', target='output2')
+    display(f'{student1.name} from {student1.section} loves the {student1.favsub} subject.', target='output')
 
 listStudent = [
     StudentAddition('Jalainie Abdullah', '10-Topaz', 'Filipino'),
